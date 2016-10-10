8142f43a6c363f888ebc2477df59a3efb3f5584d
s0113696 <cis290@PC22764.local>
2016-10-10 18:50:11 -0500

    JG

---
+++ Contact.swift
@@ -0,0 +1,17 @@
+//
+//  Contact.swift
+//  MyContacts
+//
+//  Created by cis290 on 10/10/16.
+//  Copyright © 2016 Rock Valley College. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Contact: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
